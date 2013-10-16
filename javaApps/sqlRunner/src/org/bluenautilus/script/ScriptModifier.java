eba3857dc1a7d4f4ea6e0e15e754d14fbe913f64
Beth Stevens <bstevens@peopleanswers.com>
2013-10-16 13:19:56 -0500

    Fixing sql runner to handle scripts that create stored procedures.

--- javaApps/sqlRunner/src/org/bluenautilus/script/ScriptModifier.java
+++ javaApps/sqlRunner/src/org/bluenautilus/script/ScriptModifier.java
@@ -7,11 +7,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,102 +27,150 @@ import java.nio.charset.Charset;
  */
 public class ScriptModifier implements ScriptCompletionListener {
 
-    public static final String OUTPUTFILENAME = "tempdata.txt";
-    private static final String PREFIX = "BEGIN TRANSACTION TEMP;\n\n";
-    private static final String POSTFIX = "COMMIT TRANSACTION TEMP;\n\n";
-    public File inputFile;
-    public File outputFile;
+	public static final String OUTPUTFILENAME = "tempdata.txt";
+	private static final String PREFIX = "BEGIN TRANSACTION TEMP;\n\n";
+	private static final String POSTFIX = "COMMIT TRANSACTION TEMP;\n\n";
+	//Use this string to determine whether the sql script contains a
+	//stored procedure. We parse differently if so.
+	private static final String SP_FLAG = "CREATE PROCEDURE";
+	public File inputFile;
+	public File outputFile;
 
-    public ScriptModifier(File inputFile) {
-        this.inputFile = inputFile;
 
-    }
+	public ScriptModifier(File inputFile) {
+		this.inputFile = inputFile;
 
-    @Override
-    public void scriptComplete(ScriptResultsEvent e) {
-        if (this.outputFile.exists()) {
-            //maybe later when I trust this class more
-            //   this.outputFile.delete();
-        }
-    }
+	}
 
-    public File createModifiedCopy() throws FileNotFoundException, IOException {
-        InputStream fis;
-        OutputStream outputStream;
-        BufferedReader reader = null;
-        BufferedWriter writer = null;
-        String line;
 
-        outputFile = new File(OUTPUTFILENAME);
+	@Override
+	public void scriptComplete(ScriptResultsEvent e) {
+		if (this.outputFile.exists()) {
+			//maybe later when I trust this class more
+			//   this.outputFile.delete();
+		}
+	}
 
-        try {
-            if (outputFile.exists()) {
-                outputFile.delete();
-            }
 
-            outputStream = new FileOutputStream(outputFile);
-            writer = new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8")));
-            writer.write(PREFIX);
+	public File createModifiedCopy() throws FileNotFoundException, IOException {
+		FileInputStream fis;
+		OutputStream outputStream;
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		String line;
 
-            fis = new FileInputStream(this.inputFile);
+		outputFile = new File(OUTPUTFILENAME);
+
+		try {
+			if (outputFile.exists()) {
+				outputFile.delete();
+			}
+
+			outputStream = new FileOutputStream(outputFile);
+			writer = new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8")));
+
+			fis = new FileInputStream(this.inputFile);
+
+            boolean isStoredProc = this.parseFileForStoreProcedures(fis);
+
+            //have to reset the input stream back to beginning
+            fis.getChannel().position(0);
+
             reader = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
 
-            int count = 0;
+			if (!isStoredProc) {
+				writer.write(PREFIX);
+			}
 
-            boolean munchingComments = false;
+			int count = 0;
 
-            while ((line = reader.readLine()) != null) {
+			boolean munchingComments = false;
 
-                StringBuilder newLine = new StringBuilder();
-                int totalChars = line.length();
-                int i = 0;
+			while ((line = reader.readLine()) != null) {
 
-                while(i<totalChars){
-                    char char1 = line.charAt(i);
-                    char char2 = char1;
-                    if((i+1)<totalChars){
-                       char2 = line.charAt(i+1);
-                    }
-                    if(char1=='/' && char2=='*'){
-                        munchingComments = true;
-                    }
-                    if(char1=='*' && char2=='/'){
-                        munchingComments = false;
+				StringBuilder newLine = new StringBuilder();
+				int totalChars = line.length();
+				int i = 0;
 
-                        if((i+2)>=totalChars){
-                           i=totalChars;
-                           char1 = ' ';
-                        }else{
-                            i = i+2;
-                            char1 = line.charAt(i);
-                        }
-                    }
-                    if(!munchingComments){
-                        newLine.append(char1);
-                    }
-                    i++;
-                } //end of processing string
+				while (i < totalChars) {
+					char char1 = line.charAt(i);
+					char char2 = char1;
+					if ((i + 1) < totalChars) {
+						char2 = line.charAt(i + 1);
+					}
+					if (char1 == '/' && char2 == '*') {
+						munchingComments = true;
+					}
+					if (char1 == '*' && char2 == '/') {
+						munchingComments = false;
+
+						if ((i + 2) >= totalChars) {
+							i = totalChars;
+							char1 = ' ';
+						} else {
+							i = i + 2;
+							char1 = line.charAt(i);
+						}
+					}
+					if (!munchingComments) {
+						newLine.append(char1);
+					}
+					i++;
+				} //end of processing string
+
+				writer.write(newLine.toString() + "\n");
+			}
+			if (!isStoredProc) {
+				writer.write(POSTFIX);
+			}
+
+		} finally {
+			if (null != reader) {
+				reader.close();
+			}
+			if (null != writer) {
+				writer.close();
+			}
+		}
+		return outputFile;
+	}
 
 
-                writer.write(newLine.toString() + "\n");
-            }
-            writer.write(POSTFIX);
+	private boolean clearofMarkers(String input) {
+		if (input.indexOf(PREFIX) == -1 && input.indexOf(POSTFIX) == -1) {
+			return true;
+		}
+		return false;
+	}
 
-        } finally {
-            if (null != reader) {
-                reader.close();
-            }
-            if (null != writer) {
-                writer.close();
-            }
-        }
-        return outputFile;
-    }
 
-    private boolean clearofMarkers(String input){
-        if(input.indexOf(PREFIX)==-1 && input.indexOf(POSTFIX)==-1){
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * Returns true if a stored procedure command is found
+	 * within the first 200 lines of the script.
+	 * @param fis
+	 * @return
+	 */
+	private boolean parseFileForStoreProcedures(FileInputStream fis) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+		String line;
+		int count = 0;
+
+		while (((line = reader.readLine()) != null) && (count < 200)) {
+
+			if (line != null) {
+				count++;
+				line = line.toLowerCase();
+				String flag = SP_FLAG.toLowerCase();
+                System.out.println("Looking for: " + flag + " in " + line+"\n");
+				if (line.indexOf(flag) != -1) {
+                    System.out.println("\nfound it\n\n");
+						return true;
+
+				}
+			}
+		}
+
+		return false;
+	}
+
 }
