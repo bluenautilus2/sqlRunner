f9be0f8dd618789dfc9f5a3a7dabe38b39814177
jparker <jim.parker@infor.com>
2014-05-20 16:15:37 -0500

    jdbc updates

--- javaApps/sqlRunner/src/org/bluenautilus/db/methodtype/JdbcScriptRunner.java
+++ javaApps/sqlRunner/src/org/bluenautilus/db/methodtype/JdbcScriptRunner.java
@@ -15,70 +15,74 @@ import java.util.ArrayList;
  * Script runner for JDBC connection types.
  */
 public class JdbcScriptRunner implements ScriptRunner {
-	private static final String DB_STRING = "jdbc:sqlserver://%s:%s;DatabaseName=%s";
-	// (/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(--.*[\n\r])|(^commit ?.*$)
+    private static final String DB_STRING = "jdbc:sqlserver://%s:%s;DatabaseName=%s";
 	private static final String REGEX_PARSING = "(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|(--.*[\\n\\r])";
-	private static final String REGEX_COMMIT_REMOVALS = "^commit( .*)?$";
+    private static final String REGEX_PARSING_LONG_RUNTIME = "(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|((?<=([^']*'[^'\\r\\n]*'[^-\\r\\n']*)|([^-']+))--.*)|(\\n\\r?--.*)|(^--.*)";
+    private static final String REGEX_COMMIT_REMOVALS = "^commit( .*)?$";
 	private static final String QUERY_SANDWICH = "begin transaction sqlRunnerTemp\n%s\ncommit transaction sqlRunnerTemp\n";
+    private static final String REGEX_PARSING_TEST_FOR_LONG_REQUIREMENT = "'[^-\\r\\n']*-(-+[^-\\r\\n']*)*'";
 
 
-	@Override
-	public ScriptResultsEvent runSqlCmdScript(ArrayList<ScriptCompletionListener> completionListeners, FieldItems items, SqlScriptFile scriptFile, ScriptType type) throws IOException, NoRunException {
-		String query = readInFile(scriptFile, type);
-		ScriptResultsEvent results = getScriptResultsEvent(items, scriptFile, type, query);
-
-		// if we fail, just try it again.
-		if (results.isDbProblem()) {
-			results = getScriptResultsEvent(items, scriptFile, type, query);
-		}
-		return results;
+    @Override
+	public ScriptResultsEvent runSqlCmdScript(final ArrayList<ScriptCompletionListener> completionListeners, final FieldItems items, final SqlScriptFile scriptFile, final ScriptType type) throws IOException, NoRunException {
+		return getScriptResultsEvent(items, scriptFile, type, readInFile(scriptFile, type));
 	}
 
 	/**
 	 * Perform the call to the DB.
-	 * @param items
-	 * @param scriptFile
-	 * @param type
-	 * @param query
-	 * @return
+	 * @param items Field values
+	 * @param scriptFile script file
+	 * @param type script type (rollback or regular)
+	 * @param query String query
+	 * @return script results
 	 */
-	private ScriptResultsEvent getScriptResultsEvent(FieldItems items, SqlScriptFile scriptFile, ScriptType type, String query) {
+	private ScriptResultsEvent getScriptResultsEvent(final FieldItems items, final SqlScriptFile scriptFile, final ScriptType type, final String query) {
 		boolean dbProblem = true;
 		Connection conn = null;
 		CallableStatement cs = null;
+        final StringBuilder output = new StringBuilder();
 
 		try {
 			conn = DriverManager.getConnection(String.format(DB_STRING, items.getIpAddressField(), items.getPort(), items.getDbNameField()), items.getLoginField(), items.getPasswordField());
 			conn.setAutoCommit(false);
-			String[] queries = query.split("GO\n");
+			final String[] queries = query.split("GO\n");
 
 			// Go is a transaction barrier, so split there and run each "batch" separately.
-			for (String q : queries) {
+			for (final String q : queries) {
 				// We also need to remove queries that the whole of their content is commit, we do that already.
 				if (q.trim().toLowerCase().matches(REGEX_COMMIT_REMOVALS)) {
 					continue;
 				}
 				// DDL goes in a query all of its own and all others are sandwiched in a transaction.
-				if (q.trim().toLowerCase().startsWith("create ")) {
+				if (q.trim().toLowerCase().startsWith("create ") || q.trim().toLowerCase().startsWith("alter procedure")) {
 					cs = conn.prepareCall(q);
 				} else {
 					cs = conn.prepareCall(String.format(QUERY_SANDWICH, q));
 
 				}
 				cs.execute();
-				conn.commit();
+                conn.commit();
+                // This is a dumb warning in this case.
+                @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+                SQLWarning warning = cs.getWarnings();
+                while (warning != null) {
+                    output.append(warning.getMessage());
+                    warning = warning.getNextWarning();
+                    output.append("\n");
+                }
 				cs.close();
 			}
 			dbProblem = false;
 
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			e.printStackTrace();
+            output.append(e.getMessage());
 			// First roll back the transaction
 			try {
 				if (conn != null) {
 					conn.rollback();
 				}
-			} catch (SQLException e1) {
+			} catch (final SQLException e1) {
 				e1.printStackTrace();
 			}
 
@@ -88,7 +92,7 @@ public class JdbcScriptRunner implements ScriptRunner {
 					if (!cs.isClosed()) {
 						cs.close();
 					}
-				} catch (SQLException e1) {
+				} catch (final SQLException e1) {
 					e1.printStackTrace();
 				}
 			}
@@ -97,26 +101,25 @@ public class JdbcScriptRunner implements ScriptRunner {
 			if (conn != null) {
 				try {
 						conn.close();
-				} catch (SQLException e1) {
+				} catch (final SQLException e1) {
 					e1.printStackTrace();
 				}
 			}
 		}
 
-		return new ScriptResultsEvent("JDBC mode doesn't support output", scriptFile, type, dbProblem);
+		return new ScriptResultsEvent(output.toString(), scriptFile, type, dbProblem);
 	}
 
 	/**
 	 * Read in the script and parse it for un-needed stuff.
-	 * @param scriptFile
-	 * @param type
-	 * @return
+	 * @param scriptFile script file
+	 * @param type type of script (rollback or regular)
+	 * @return String value of the file contents
 	 * @throws IOException
-	 * @throws NoRunException
 	 */
-	private String readInFile(SqlScriptFile scriptFile, ScriptType type) throws IOException, NoRunException {
+	private String readInFile(final SqlScriptFile scriptFile, final ScriptType type) throws IOException {
 		File file = null;
-		StringBuilder sb = new StringBuilder();
+		final StringBuilder sb = new StringBuilder();
 
 		if(ScriptType.REGULAR == type){
 			file = scriptFile.getTheFile();
@@ -150,10 +153,14 @@ public class JdbcScriptRunner implements ScriptRunner {
 
 	/**
 	 * Do the script parsing.
-	 * @param sb
-	 * @return
+	 * @param sb unparsed script
+	 * @return parsed script
 	 */
-	private String additionalParsing(StringBuilder sb) {
-		return sb.toString().replaceAll(REGEX_PARSING, " ");
+	private String additionalParsing(final StringBuilder sb) {
+        if (sb.toString().matches(REGEX_PARSING_TEST_FOR_LONG_REQUIREMENT)) {
+            return sb.toString().replaceAll(REGEX_PARSING_LONG_RUNTIME, " ");
+        } else {
+            return sb.toString().replaceAll(REGEX_PARSING, " ");
+        }
 	}
 }
